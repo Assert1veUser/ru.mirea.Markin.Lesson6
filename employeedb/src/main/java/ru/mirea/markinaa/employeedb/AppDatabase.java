6b47a47197da0a4e76d446328b5f90a330c1e819
User <tony.markin2017@yande.ru>
2023-05-17 14:52:17 +0300

    init

--- employeedb/src/main/java/ru/mirea/markinaa/employeedb/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.markinaa.employeedb;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {SuperHero.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract SuperHeroDao superHeroDao();
-}
